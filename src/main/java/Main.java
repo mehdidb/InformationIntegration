5c0800bdb90bd3d094a71cc1ea311c6a7bb81e44
mkobbi <mahmoud.kobbi@hotmail.fr>
2018-01-19 17:13:47 +0100

    Résolution du problème SLF4J

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,11 @@
-public class Main {
+import org.apache.jena.query.Dataset;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.RDFDataMgr;
 
+public class Main {
+    public static void main(String[] args){
+        Model model = RDFDataMgr.loadModel("data/ontology_restaurant1.owl");
+        Dataset dataset = RDFDataMgr.loadDataset("data/restaurant1.rdf") ;
+        System.out.println(model.size());
+    }
 }
