3887eaea6185f04267f8ea541c43a64858893e47
mehdidb <mehdi.deboub@supcom.tn>
2018-01-19 17:35:51 +0100

    First version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
 import org.apache.jena.query.Dataset;
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
 
 public class Main {
     public static void main(String[] args){
-        Model model = RDFDataMgr.loadModel("data/ontology_restaurant1.owl");
-        Dataset dataset = RDFDataMgr.loadDataset("data/restaurant1.rdf") ;
+        Model model = RDFDataMgr.loadModel("data/ontology_restaurant1_rdf.owl");
+        //Dataset dataset = RDFDataMgr.loadDataset("data/restaurant1.rdf") ;
         System.out.println(model.size());
     }
 }
