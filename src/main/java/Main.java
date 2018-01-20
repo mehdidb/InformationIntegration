7b6e03d436ff99cbd966284e6e587037d088c83c
mehdidb <mehdi.deboub@supcom.tn>
2018-01-20 16:24:56 +0100

    Second version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,38 @@
 import org.apache.jena.query.Dataset;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.RDFList;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
 
 public class Main {
     public static void main(String[] args){
+        System.out.println("The model is :");
         Model model = RDFDataMgr.loadModel("data/ontology_restaurant1_rdf.owl");
-        Dataset dataset = RDFDataMgr.loadDataset("data/restaurant1.rdf") ;
+        RDFDataMgr.read(model, "data/restaurant1.rdf");
+        RDFDataMgr.write(System.out, model, Lang.RDFXML) ;
+
+        //System.out.println("The model from dataset is :");
+        //Dataset data = RDFDataMgr.loadDataset("data/restaurant1.rdf");
+        //RDFDataMgr.write(System.out, data.getDefaultModel(), Lang.RDFXML) ;
+
+
+        //model1.
+        //RDFDataMgr.read(model1, "data/restaurant1.rdf") ;
+        //RDFList liste = model1.createList();
+        //System.out.println(liste.size());
+        //for (RDFNode node : liste.asJavaList()) {
+        //    System.out.println("1");
+        //}
+        //Model model2 = RDFDataMgr.loadModel("data/ontology_restaurant2_rdf.owl");
+
+
+        //Dataset dataset = RDFDataMgr.loadDataset() ;
         // RDFDataMgr.read(model, "data/restaurant2.rdf");
-        dataset.setDefaultModel(model);
-        System.out.println();
-        System.out.println(model.size());
+        //dataset.setDefaultModel(model1);
+        //System.out.println(model1.isIsomorphicWith(dataset.getDefaultModel()));
+        //System.out.println(model1.size());
+        //System.out.println(model2.size());
     }
 }
